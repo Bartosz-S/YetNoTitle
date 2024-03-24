832021ce21629b451bc9b9950373e5a8a1436eef
Bartosz <88800970+Bartosz-S@users.noreply.github.com>
2024-03-24 21:20:37 +0100

    pressing for debug logs

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField, Delayed] private float movementSpeed = 10;
+    
     private Vector2 movementInput;
+    private bool isActButtPressed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +21,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(movementInput.x, movementInput.y, 0) * movementSpeed * Time.deltaTime);
+        transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * movementSpeed * Time.deltaTime);
+        if(isActButtPressed) { Debug.Log("Action button pressed!"); isActButtPressed = false; }
     }
 
     public void OnMovement(InputAction.CallbackContext context) => movementInput = context.ReadValue<Vector2>();
+
+    public void OnInteraction(InputAction.CallbackContext context) => isActButtPressed = true;
+   
+    
 }
