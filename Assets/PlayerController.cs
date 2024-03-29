c4e9dbc31e812cf17e585352238a7d2790691c90
Bartosz <88800970+Bartosz-S@users.noreply.github.com>
2024-03-29 11:40:02 +0100

    update 1

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,27 +7,85 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField, Delayed] private float movementSpeed = 10;
-    
-    private Vector2 movementInput;
-    private bool isActButtPressed = false;
+    [SerializeField, Delayed] private float movementSpeed;
 
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 movementInput;
+    private Controls controls;
+    [SerializeField] private Player choosePlayer;
+    private enum Player
     {
-        
+        None,
+        Player1,
+        Player2,
+    }
+
+    private void Awake()
+    {
+        switch (choosePlayer)
+        {
+            case Player.None:
+                break;
+            case Player.Player1:
+                controls = new Controls();
+                controls.Enable();
+                break;
+            case Player.Player2:
+                controls = new Controls();
+                controls.Enable();
+                break;
+        }
+    }
+
+    private void OnEnable()
+    {
+        ActivatePlayers();
+    }
+
+    private void OnDisable()
+    {
+        DeactivatePlayers();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * movementSpeed * Time.deltaTime);
-        if(isActButtPressed) { Debug.Log("Action button pressed!"); isActButtPressed = false; }
-    }
+        transform.Translate(movementSpeed * Time.deltaTime * new Vector2(movementInput.x, movementInput.y));
+    } 
 
     public void OnMovement(InputAction.CallbackContext context) => movementInput = context.ReadValue<Vector2>();
 
-    public void OnInteraction(InputAction.CallbackContext context) => isActButtPressed = true;
-   
-    
+    public void OnInteraction(InputAction.CallbackContext context) => Debug.LogWarning("Action button pressed!");
+
+    public void ActivatePlayers()
+    {
+        controls.Gameplay.Enable();
+        switch (choosePlayer)
+        {
+            case Player.Player1:
+                controls.Gameplay.InteractionP1.performed += OnInteraction;
+                controls.Gameplay.MoveP1.performed += OnMovement;
+                break;
+            case Player.Player2:
+                controls.Gameplay.InteractionP2.performed += OnInteraction;
+                controls.Gameplay.MoveP2.performed += OnMovement;
+                break;
+        }
+    }
+
+    public void DeactivatePlayers()
+    {
+        controls.Gameplay.Disable();
+        switch (choosePlayer)
+        {
+            case Player.Player1:
+                controls.Gameplay.InteractionP1.performed -= OnInteraction;
+                controls.Gameplay.MoveP1.performed -= OnMovement;
+                break;
+            case Player.Player2:
+                controls.Gameplay.InteractionP2.performed -= OnInteraction;
+                controls.Gameplay.MoveP2.performed -= OnMovement;
+                break;
+        }
+    }
+
 }
