a7f417f80d65c823aeabc3bed1a62e3398eb6973
PlazmaDrink <Roma.Marchenko@gmail.com>
2024-06-02 23:34:32 +0200

    Added Animations
    Still to polish stuff

--- Assets/Players/PlayerVisuals.cs
+++ Assets/Players/PlayerVisuals.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class PlayerVisuals : MonoBehaviour
 {
@@ -9,10 +10,36 @@ public class PlayerVisuals : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private SpriteRenderer spriteRenderer;
     bool lastFlipX;
+    private Controls controls;
+    private bool isHoldingBlock;
 
     private void Start() {
         playerReferenceContainer.PlayersHealthSystem.onHealthChange.AddListener(TakeDamageAnimationTrigger);
+        controls = new Controls();
+        controls.Gameplay.Enable();
+        controls.Gameplay.InteractionP1.performed += InteractionP1_performed;
+        controls.Gameplay.InteractionP2.performed += InteractionP2_performed;
     }
+
+    private void InteractionP2_performed(UnityEngine.InputSystem.InputAction.CallbackContext context) {
+        if (context.performed) {
+            animator.SetTrigger("Block");
+            isHoldingBlock = true;
+            animator.SetBool("IsBlockHolding", true);
+        }
+
+        if (context.canceled) {
+            isHoldingBlock = false;
+            animator.SetBool("IsBlockHolding", false);
+        }
+    }
+
+    private void InteractionP1_performed(UnityEngine.InputSystem.InputAction.CallbackContext context) {
+        if (context.performed) {
+            animator.SetTrigger("Attack");
+        }
+    }
+
     private void Update() {      
         if (playerReferenceContainer.PlayersInput.MovementInput != new Vector2(0, 0)) {
 
@@ -26,6 +53,7 @@ public class PlayerVisuals : MonoBehaviour
         } else {
             animator.SetBool("IsWalking", false);
         }
+        
     }
 
     private void TakeDamageAnimationTrigger() {
