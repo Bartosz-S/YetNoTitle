ee2de048a4904bd594257e5694c959edd588b474
MiDz5 <126875697+MiDz5@users.noreply.github.com>
2024-06-02 23:45:38 +0200

    Simplified TrapProjectile script

--- Assets/Scripts/ShootingTrapProjectile.cs
+++ Assets/Scripts/ShootingTrapProjectile.cs
@@ -6,20 +6,12 @@ using UnityEngine;
 public class ShootingTrapProjectile : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.CompareTag("Enemies") || collision.gameObject.CompareTag("Projectiles"))
-        {
-            // Ignore future collisions between this collider and the collided object's collider
-            Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
-        }
-        else
-        {
+        
             if(collision.gameObject.TryGetComponent<PlayerHealthSystem>(out PlayerHealthSystem layerHealthSystem)) {
                 PlayerHealthSystem playerHealthSystem = collision.gameObject.GetComponent<PlayerHealthSystem>();
                 playerHealthSystem.TakeDamage(10);
             };
         Destroy(gameObject);
-
-        }
 
     }
 
